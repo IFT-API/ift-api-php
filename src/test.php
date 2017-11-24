950d771e9827f598570511be5923cd563ae6c620
Théo-Paul <theo-paul.haezebrouck@acta.asso.fr>
2017-11-24 15:23:08 +0100

    Comment added

--- src/test.php
+++ src/test.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 require '../vendor/autoload.php';
-$client = new \GuzzleHttp\Client(['verify' => false]);
+$client = new \GuzzleHttp\Client(['verify' => false]); // verify = false added to avoid SSL problem
 ?>
 <html>
  <head>
@@ -12,47 +12,47 @@ $client = new \GuzzleHttp\Client(['verify' => false]);
 
  <p><strong>GET /api/hello :</strong></p>
  <?php
- 
+
  $res = $client->get('https://ift-api.herokuapp.com/api/hello');
  echo '<p>'.json_decode($res->getBody())->message.'</p>'
- 
+
  ?>
- 
- 
- 
+
+
+
  <p><strong>GET /api/campagnes :</strong></p>
  <?php
- 
+
  $res = $client->get('https://ift-api.herokuapp.com/api/campagnes');
  echo '<p>'.$res->getBody().'</p>'
- 
+
  ?>
- 
- 
- 
+
+
+
  <p><strong>GET /api/campagnes/2017 :</strong></p>
  <?php
- 
+
  $res = $client->get('https://ift-api.herokuapp.com/api/campagnes/2017');
  echo '<p>'.$res->getBody().'</p>'
- 
+
  ?>
- 
- 
- 
+
+
+
  <p><strong>GET /api/produits-doses-reference?campagneId={campagneId}&cultureId={cultureId}&produitId={produitId} :</strong></p>
  <?php
- 
+
  $res = $client->get('https://ift-api.herokuapp.com/api/produits-doses-reference?campagneId=e02b1e37-669b-497e-be4c-e8b9359cb587&cultureId=6f288e46-ead7-491d-9213-b498dcf82329&produitId=64fa2f66-9cd6-46af-8ff0-2882edcfd869');
  echo '<p>'.$res->getBody().'</p>'
- 
+
  ?>
 
 
 
  <p><strong>GET /api/ift/traitement?campagneId={campagneId}&numeroAmmId={numeroAmmId}&cultureId={cultureId}&cibleId={cibleId}&traitementId={traitementId}&uniteId={uniteId}&dose={dose}&facteurDeCorrection={facteurDeCorrection} :</strong></p>
  <?php
- 
+
  $res = $client->get('https://ift-api.herokuapp.com/api/ift/traitement?'
     .'campagneId=e02b1e37-669b-497e-be4c-e8b9359cb587'
     .'&numeroAmmId=d76038c9-c456-4487-91ea-3fa1a8853978'
@@ -64,7 +64,7 @@ $client = new \GuzzleHttp\Client(['verify' => false]);
     .'&facteurDeCorrection=100'
 );
  echo '<p>'.$res->getBody().'</p>'
- 
+
  ?>
  </body>
 </html>
